e7a0669ffff6d355245b433f330f2df519516de5
xiaocuixt <cjw624923@126.com>
2015-11-10 15:58:03 +0800

    some pages

--- app/controllers/importants_controller.rb
+++ app/controllers/importants_controller.rb
@@ -1,4 +1,12 @@
 class ImportantsController < ApplicationController
   def index
   end
+
+  #未来降水
+  def future
+  end
+
+  #台风路径
+  def routing
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
   resources :scenes, only: :index
-  resources :importants, only: :index
+  resources :importants, only: :index do
+    collection do
+      get :routing
+      get :future
+    end
+  end
   resources :histories, only: :index do
     collection do
       get :list
