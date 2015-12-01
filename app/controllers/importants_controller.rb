c5bbbc78e93713fbc5cf2a2ee6fbad33bdfb183b
xiaocuixt <cjw624923@126.com>
2015-12-01 23:08:45 +0800

    fulture rain

--- app/controllers/importants_controller.rb
+++ app/controllers/importants_controller.rb
@@ -18,9 +18,9 @@ class ImportantsController < ApplicationController
   #未来降水
   def future
     #@points = FileParse.parse_all.reject{|x| x['count'] == 0.0}
-    @points = FileParse.parse_all
+    #@points = FileParse.parse_all
+    @points = [{ "lng"=>121.24, "lat"=>31.4, "count"=>0.8},{ "lng"=>121.24, "lat"=>31.4, "count"=>0.0}]
     gon.points = @points.to_json
-    # @points = [{ "lng"=>121.24, "lat"=>31.4, "count"=>0.8},{ "lng"=>121.24, "lat"=>31.4, "count"=>0.0}]
     # gon.points = @points.to_json
   end
 
