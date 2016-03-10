25c49e3fcb7a3d63c0d3deaedd66248cee378b54
xiaocuixt <cjw624923@126.com>
2016-03-10 09:58:02 +0800

    修改接口

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @now = Time.now.strftime("%Y-%m-%d")
-    @weather_info_first = Weather.get_weather_data['data'][0]
+    @weather_info_first = Weather.get_weather_data['data']['items'][0]
 
-    @weather_info = Weather.get_weather_data['data'][1..-2]  #页面需要，显示未来八天的天气
+    @weather_info = Weather.get_weather_data['data']['items'][1..-2]  #页面需要，显示未来八天的天气
     gon.weather_info = @weather_info
 
     @aqi_info = Weather.get_aqi_data['data']
